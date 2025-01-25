dc6cfb6f6a57086085d31ff7334651de1fa04418
ytarmy1321 <33156820+ytarmy1321@users.noreply.github.com>
2025-01-26 02:45:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,134 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
+const songs = [
+  {
+    title: "İntro",
+    name: "Ati242",
+    source:"song-list/intro.mp3",
+  },
+  {
+    title: "Love Gun",
+    name: "Heijan & Muti",
+    source:"song-list/lovegun.mp3",
+  },
+  {
+    title: "anlatamam",
+    name: "Muti",
+    source:"song-list/anlatamam.mp3",
+  },
+  {
+    title: "Telefon",
+    name: "Ati242",
+    source:"song-list/telefon.mp3",
+  },
+  {
+    title: "Ait Hissetmiyorum",
+    name: "Ati242",
+    source:"song-list/aithissetmiyorum.mp3",
+  },
+
+  {
+    title: "GELME İSTEMEM",
+    name: "BLOK3",
+    source:"song-list/gelmeistemem.mp4",
+  },
+  {
+    title: "İNTİHAR",
+    name: "Era7capone ft. NASTEISHA",
+    source:"song-list/intihar.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
+
+// Inspiration: https://dribbble.com/shots/5455156-Car-HMI-assistant-Album-switching
